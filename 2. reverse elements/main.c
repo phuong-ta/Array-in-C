4005834e6b196138d1094bfebe6add9caa11d439
phuong-ta <phuong-ta@users.noreply.github.com>
2022-06-28 20:26:17 +0300

    xercise 2 done

---
+++ 2. reverse elements/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* code */
+
+    printf("enter 10 numbers");
+    int store[10];
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+        scanf("%d", &store[i]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+        printf("%d\n", store[i]);
+    }
+        for (int i = 9; i >=0; i--)
+    {
+        /* code */
+        printf("%d\n", store[i]);
+    }
+    
+    return 0;
+}
